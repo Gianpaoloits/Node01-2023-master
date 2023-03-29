6babab5e95fffdea9c27725c114f75915cd0ad2b
Gianpaoloits <gianpaolo.gabancho.its@gmail.com>
2023-03-29 12:11:23 +0200

    commint1

--- modules/math.js
+++
@@ -1,6 +0,0 @@
-export function add(a, b){ return a+b; }
-export function sub(a, b){ return a-b; }
-
-function privateadd(a, b){ return a+b; }
-
-export default function addDefault(a,b){ return a+b; }
